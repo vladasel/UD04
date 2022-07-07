32c04a7b05c1af0184eb8a84e0d84766399142ab
ionela vladasel <ionelavladasel2710@gmail.com>
2022-07-07 11:31:24 +0200

    done ej4

--- UD04/src/ej4.java
+++ UD04/src/ej4.java
@@ -2,8 +2,11 @@
 public class ej4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int n = 4;
+		System.out.println("valor inicial n = "+n);
+		System.out.println("n + 77 = "+(n+77));
+		System.out.println("n - 3 = "+(n-3));
+		System.out.println("n * 2 = "+(n*2));
 	}
 
 }
