93160947390823bc185f3ede3a53008c17e7bfcb
ionela vladasel <ionelavladasel2710@gmail.com>
2022-07-07 10:25:53 +0200

    ej1

---
+++ UD04/src/ej1.java
@@ -0,0 +1,18 @@
+
+public class ej1 {
+
+	public static void main(String[] args) {
+		int num1 = 2;
+		int num2 = 4;
+		int suma = num1 + num2;
+		int resta = num1 - num2;
+		int mult = num1 * num2;
+		int div = num1 / num2;
+		int mod = num1 % num2;
+		System.out.println("suma " + suma + "\nresta " + resta + "\nmultiplicación " + mult + "\ndivisión " + div
+				+ "\nmodulo " + mod);
+
+
+	}
+
+}
