12077a2c51c695ae780e071fa5a8ddb8d8c5abcb
ionela vladasel <ionelavladasel2710@gmail.com>
2022-07-07 10:40:09 +0200

    ej2

--- UD04/src/ej2.java
+++ UD04/src/ej2.java
@@ -2,7 +2,13 @@
 public class ej2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int N=4;
+		double A=6.5;
+		char C='a';
+		System.out.println("el valore de las variables es: \nN = "+N+"\nA = "+A+"\nC = "+C);
+		System.out.println("suma N + A \n "+N+" + "+A+" = "+(N+A) );
+		System.out.println("diferencia A - N \n"+A+" - "+N+" = "+(A-N));
+		System.out.println("valor numerico del cáracter "+C+" = "+ (int)C);
 
 	}
 
--- UD04/src/ej5.java
+++ UD04/src/ej5.java
@@ -2,8 +2,7 @@
 public class ej5 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 }
