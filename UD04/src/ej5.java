529d4467c73598dd4c6ce28b7c9c5ac097173c69
ionela vladasel <ionelavladasel2710@gmail.com>
2022-07-07 11:36:40 +0200

    done ej5

--- UD04/src/ej5.java
+++ UD04/src/ej5.java
@@ -2,7 +2,15 @@
 public class ej5 {
 
 	public static void main(String[] args) {
-		
+		int a = 1;
+		int b = 2;
+		int c = 3;
+		int d = 4;
+		b = c;
+		c = a;
+		a = d;
+		d = b;
+		System.out.println(a+" "+b+" "+c+" "+d);
 	}
 
 }
