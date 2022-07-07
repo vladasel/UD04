49b7e73c834e421e58f647d0e6d118d48b914fd1
ionela vladasel <ionelavladasel2710@gmail.com>
2022-07-07 11:23:49 +0200

    done ej3

--- UD04/src/ej3.java
+++ UD04/src/ej3.java
@@ -5,7 +5,31 @@ public class ej3 {
 		int y = 4;
 		double n = 5.5;
 		double m = 8.23;
-		
+		System.out.println("valor de las variables\nx = " + x + "\ny = " + y + "\nn = " + n + "\nm = " + m);
+		System.out.println("******************************************");
+		System.out.println("suma x + y = " + (x + y));
+		System.out.println("diferencia x -y = " + (x - y));
+		System.out.println("producto x * y = " + (x * y));
+		System.out.println("cociente x / y = " + (x / y));
+		System.out.println("resto x % y = " + (x % y));
+		System.out.println("******************************************");
+		System.out.println("suma n + m = " + (n + m));
+		System.out.println("diferencia n - m = " + (n - m));
+		System.out.println("producto n * m = " + (n * m));
+		System.out.println("cociente n / m = " + (n / m));
+		System.out.println("resto n % m = " + (n % m));
+		System.out.println("******************************************");
+		System.out.println("suma x + n = " + ((double) x + n));
+		System.out.println("cociente x / m = " + ((double) x / m));
+		System.out.println("resto y % m = " + (x % m));
+		System.out.println("******************************************");
+		System.out.println("doble de las variables");
+		System.out.println("x = " + x * 2);
+		System.out.println("y = " + y * 2);
+		System.out.println("n = " + n * 2);
+		System.out.println("m = " + m * 2);
+		System.out.println("suma de las variables = " + ((double) x + (double) y + n + m));
+		System.out.println("producto de las variables = "+(double)x*(double)y*n*m);
 
 	}
 }
